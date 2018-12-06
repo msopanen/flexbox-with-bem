ed1a9b88bc27fc4df29cb1a588e537a76dfd0c63
msopanen <mikko.p.sopanen@gmail.com>
2018-12-06 21:39:03 +0200

    Added infinite scroll class xx
    Xd# Please enter the commit message for your changes. Lines starting

--- src/App.js
+++ src/App.js
@@ -26,6 +26,54 @@ const Body = ({count}) => {
   );
 } 
 
+class ScrollingBody extends Component {
+  constructor(props) {
+    super(props);
+
+    const images = [];
+
+    for(let i=0; i<10; i++) {
+      images.push(<img src={logo} className="body--logo" alt="logo" />);
+    };
+
+    this.state = {
+      images: images
+    }
+  }
+
+  scrollHandler = (e) => {
+    if(isTop(e)) {
+      console.log("Top reached")
+    } else if (isBottom(e)) {
+      console.log("Bottom reached")
+
+      const images = [];
+
+      for(let i=0; i<10; i++) {
+        images.push(<img src={logo} className="body--logo" alt="logo" />);
+      };
+
+      this.setState({
+        images: [...this.state.images, ...images]
+      });
+    }
+  }
+
+  render() {
+
+    const { images } = this.state;
+    
+    return (
+      <div className="app--body" onScroll={this.scrollHandler}>
+        {this.state.images}
+      </div>
+    );
+  }
+}
+
+const isTop = e => e.target.scrollTop === 0;
+const isBottom = e => e.target.scrollHeight - e.target.scrollTop === e.target.clientHeight;
+
 const Footer = () => {
   return (
     <footer className="app--footer">
@@ -34,13 +82,12 @@ const Footer = () => {
   );
 } 
 
-
 class App extends Component {
   render() {
     return (
       <div className="app">
         <Header/>
-        <Body count={70}/>
+        <ScrollingBody/>
         <Footer/>
       </div>
     );
